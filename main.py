f40e523a6800d5e820b673b05437946ae6c5e34e
Ilia <zaicne@inbox.ru>
2021-07-28 20:15:08 +0200

    get_symbols_lists

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+import pprint
+
+REQ_CONST_OKEX = 'https://www.okex.com'
+REQ_CONST_BINANCE = 'https://api.binance.com'
+req_method_okex = '/api/v5/public/instruments'
+req_method_binance = '/api/v3/exchangeInfo'
+url_binance = REQ_CONST_BINANCE + req_method_binance
+url_okex = REQ_CONST_OKEX + req_method_okex
+params_okex = {
+    'instType': 'SPOT'
+}
+
+okex_instruments = []
+response_okex = requests.get(url_okex, params=params_okex)
+for instrument in response_okex.json()['data']:
+    okex_instruments += [[instrument['instId'], instrument['baseCcy'], instrument['quoteCcy']]]
+print(okex_instruments)
+pprint.pprint(response_okex.json())
+
+binance_instruments = []
+response_binance = requests.get(url_binance)
+for instrument in response_binance.json()['symbols']:
+    binance_instruments += [[instrument['symbol'], instrument['baseAsset'], instrument['quoteAsset']]]
+pprint.pprint(response_binance.json())
+
+with open('response_okex.txt', 'w') as f:
+    f.write(str(okex_instruments))
+with open('response_binance.txt', 'w') as f:
+    f.write(str(binance_instruments))
